37500d29bf862ee945ebf8cef04bd2193f1c6f74
avinash7654 <kumaravinashsingh7654@gmail.com>
2022-08-15 17:53:13 +0530

    removed commment

--- src/Scanner.java
+++ src/Scanner.java
@@ -2,7 +2,7 @@ import java.io.InputStream;
 
 public class Scanner {
 	public Scanner(InputStream in) {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public static void main(String[] args) {
