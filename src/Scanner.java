120daa27a615db3cb79b4675ab147c8f6b7c2459
avinash7654 <kumaravinashsingh7654@gmail.com>
2022-08-15 17:47:09 +0530

    new program for scanner

---
+++ src/Scanner.java
@@ -0,0 +1,30 @@
+import java.io.InputStream;
+
+public class Scanner {
+	public Scanner(InputStream in) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+	java.util.Scanner sc =new java.util.Scanner(System.in);	
+		System.out.println("enter the name: ");
+		String name=sc.next();
+		System.out.println("enter the age: ");
+		int age=sc.nextInt();
+	System.out.println("enter the name: ");
+		char Gender=sc.next().charAt(0);
+		System.out.println("enter the number: ");
+		long number=sc.nextLong();
+		System.out.println("____");
+		System.out.println("name:"+name);
+		System.out.println("age:"+age);
+		System.out.println("Gender:"+Gender);
+		System.out.println("number:"+number);
+		
+	}
+
+	public int nextint() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
